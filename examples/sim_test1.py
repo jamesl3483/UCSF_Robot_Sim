09aa8f38c19063e5d6b37434ad7b0cec7b058352
jamesl3483 <jlukas6249@gmail.com>
2024-03-07 14:20:53 -0800

    try again

--- examples/sim_test1.py
+++ examples/sim_test1.py
@@ -18,43 +18,39 @@ jaco = Jaco()
 gipper = JacoGripper(0)
 target = Shape('Cuboid')
 current_pos = jaco.get_position()
-print(current_pos)
+
 # target = np.array([0.25, 0.25, 0.25]
-#path = jaco.get_path(position=target.get_position(), quaternion=jaco.get_quaternion())
-#path = agent.get_linear_path(position=pos, angle=0)
+# path = jaco.get_path(position=target.get_position(), quaternion=jaco.get_quaternion())
+# path = agent.get_linear_path(position=pos, angle=0)
 increment = 0.01
 
-if key == keyboard.Key.up:
-    position[1] += increment  # Move in positive Y direction
-elif key == keyboard.Key.down:
-    position[1] -= increment  # Move in negative Y direction
-elif key == keyboard.Key.left:
-    position[0] -= increment  # Move in negative X direction
-elif key == keyboard.Key.right:
-    position[0] += increment  # Move in positive X direction
-elif key == keyboard.KeyCode.from_char('w'):
-    position[2] += increment  # Move up in Z direction
-elif key == keyboard.KeyCode.from_char('s'):
-    position[2] -= increment  # Move down in Z direction
 
+def move(key):
+    if key == keyboard.Key.up:
+        current_pos[1] += increment  # Move in positive Y direction
+    elif key == keyboard.Key.down:
+        current_pos[1] -= increment  # Move in negative Y direction
+    elif key == keyboard.Key.left:
+        current_pos[0] -= increment  # Move in negative X direction
+    elif key == keyboard.Key.right:
+        current_pos[0] += increment  # Move in positive X direction
+    elif key == keyboard.KeyCode.from_char('w'):
+        current_pos[2] += increment  # Move up in Z direction
+    elif key == keyboard.KeyCode.from_char('s'):
+        current_pos[2] -= increment  # Move down in Z direction
 
 
 while True:
 
 
     path.visualize()
-
+    path = jaco.get_path(position=current_pos, quaternion=jaco.get_quaternion())
     done = False
     while not done:
         done = path.step()
         pr.step()
 
     path.clear_visualization()
-done = False
-
-while not done:
-    done = path.step()
-    pr.step()
 
 print('Done ...')
 input('Press enter to finish ...')
